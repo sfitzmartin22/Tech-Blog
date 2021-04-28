665f71fdd46cf03c0079f2d76463e99d2f6f1750
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-04-28 00:18:25 -0400

    updated the user model with the bcrypt code

--- models/User.js
+++ models/User.js
@@ -4,7 +4,11 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize User model (table) by extending off Sequelize's Model class
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // set up fields and rules for Product model
 User.init(
@@ -32,6 +36,16 @@ User.init(
     },
   },
   {
+    hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
