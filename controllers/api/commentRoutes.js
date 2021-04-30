01f1ca280ce91c938ea65ce8cfd36fa5eac1b317
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-04-30 01:56:18 -0400

    more controller additions

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { Comment } = require("../../models")
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newComment);
+        } catch (err) {
+            res.status(400).json(err);
+        }; 
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const blogComment = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        
+        if(!blogComment) {
+            res.status(404).json({ message: "There is no comment on this blog!"});
+            return;
+        }
+
+        res.status(200).json(blogComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
