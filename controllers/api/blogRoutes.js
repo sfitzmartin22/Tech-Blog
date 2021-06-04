eba8a8904e68341eecb469a55b3cab5cb9eb8ad7
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-06-03 20:23:39 -0400

    handlebars editing

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -4,15 +4,20 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
     try {
-        const newBlog = await Blog.create({
-            ...req.body,
-            user_id: req.session.user_id,
-        });
-
+        const newBlog = await Blog.create(req.body);
         res.status(200).json(newBlog);
-        } catch (err) {
-            res.status(400).json(err);
+    } catch (err) {
+        res.status(400).json(err);
         }; 
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll();
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -39,11 +39,11 @@ User.init(
   {
     hooks: {
         beforeCreate: async (newUserData) => {
-          newUserData.password = await bcrypt.hash(newUserData.password, 20);
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
           return newUserData;
         },
         beforeUpdate: async (updatedUserData) => {
-          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 20);
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
           return updatedUserData;
         },
     },
